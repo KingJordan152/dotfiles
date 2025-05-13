8636dea766fc4eefc9e6da0c5c42962574a15822
Jordan King <kingjordan2718@gmail.com>
2025-05-13 16:10:30 -0700

    feat(nvim): Created Treesitter overrides for TypeScript & JavaScript
    In order for Treesitter to correctly color most constant variables, I
    had to add these overrides. By default, Treesitter only marks a TS/JS
    variable as `@constant` if you capitalize the variable name, which isn't
    a common convention in those languages.
    
    Hopefully this carries over to other languages. If not, I'll just remove
    these `highlights` files and add specific overrides within the
    `tokyonight` file.

---
+++ nvim/.config/nvim/queries/typescript/highlights.scm
@@ -0,0 +1,43 @@
+;; extends
+; Code primarily written by Chat-GPT
+
+; By default, in TypeScript, a variable is only recognized as a `@constant` if it's written in all caps,
+; NOT if it's actually declared with `const`. These overrides ensure these variables are properly
+; recognized/highlighted.
+
+; Ensure all variables declared with `const` are correctly recognized and highlighted as `@constant`
+(
+  (lexical_declaration
+    (variable_declarator
+      name: (identifier) @constant
+    )
+  )
+  (#set! "priority" 100)
+)
+
+; Apply `@constant` to array-destructured constant variables
+(
+  (lexical_declaration
+    (variable_declarator
+      name: (array_pattern
+              (identifier) @constant
+              (_)? @constant)
+    )
+  )
+  (#set! "priority" 100)
+)
+
+; Since functions can be declared with `const`, ensure the `@function` identifier has a higher priority than `@constant`
+(
+  (lexical_declaration
+    (variable_declarator
+      name: (identifier) @function
+      value: [
+        (arrow_function)
+        (function_expression)
+      ]
+    )
+  )
+  (#set! "priority" 110)
+)
+
