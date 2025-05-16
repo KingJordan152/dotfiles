7e5278696ee64d96278d8f64caed0440571d7c40
Jordan King <kingjordan2718@gmail.com>
2025-05-15 17:13:13 -0700

    feat(nvim): Configured linting via nvim-lint
    I recently learned that LSP diagnostics and linting are two completely
    different things, which means that they need to be configured separately
    within Neovim.
    
    Fortunately, nvim-lint is very similar to conform, which is essentially
    the same plugin but for formatting. nvim-lint is also quite nice because
    it pushes linting errors into `vim.diagnostic` so that they can be
    viewed as inline errors.

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -6,12 +6,10 @@ return {
 
 	keys = {
 		{
-			-- Customize or remove this keymap to your liking
 			"<leader>f",
 			function()
 				require("conform").format({ async = true })
 			end,
-			mode = "",
 			desc = "Format buffer",
 		},
 	},
---
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,36 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			svelte = { "eslint_d" },
+			python = { "pylint" },
+		}
+
+		-- Configure autocmd for automatic linting
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+
+	keys = {
+		{
+			"<leader>l",
+			function()
+				require("lint").try_lint()
+			end,
+			desc = "Lint buffer",
+		},
+	},
+}
