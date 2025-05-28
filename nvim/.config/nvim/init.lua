6ec8d761605b20667f2a6ad46def4660d5ddb8cc
Jordan King <kingjordan2718@gmail.com>
2025-05-27 20:31:06 -0700

    feat(nvim): Removed unnecessary code for enabling LSPs
    Per my last commit, I've deleted the code responsible for manually
    enabling the LSPs located in my `lsp` directory. This is now handled
    fully by `mason-lspconfig`.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,3 @@
 require("config.lazy")
 require("current-theme")
 require("options")
-
-local lsp_configs = {}
-
-for _, f in pairs(vim.api.nvim_get_runtime_file("lsp/*.lua", true)) do
-	local lsp_name = vim.fn.fnamemodify(f, ":t:r")
-	table.insert(lsp_configs, lsp_name)
-end
-
-vim.lsp.enable(lsp_configs)
