3c21bf1d90f9680ff6cee783139674759cf941cb
Jordan King <kingjordan2718@gmail.com>
2025-05-27 20:43:34 -0700

    style(nvim): Changed `lualine` powerline characters

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -8,8 +8,9 @@ return {
 	opts = {
 		options = {
 			theme = "auto",
-			component_separators = { left = "/", right = "/" },
-			section_separators = { left = "", right = "" },
+			component_separators = { left = "\\", right = "/" },
+			section_separators = { left = "", right = "" },
+			globalstatus = true,
 		},
 		sections = {
 			lualine_a = {
