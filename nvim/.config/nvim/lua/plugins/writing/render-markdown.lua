0d2d1faec2f29c37dfc5b4105795661c23da3633
Jordan King <kingjordan2718@gmail.com>
2025-05-27 20:42:46 -0700

    feat(nvim): Added additional customizations to `render-markdown`

--- nvim/.config/nvim/lua/plugins/writing/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/writing/render-markdown.lua
@@ -1,21 +1,39 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	ft = { "markdown", "gitcommit" },
 
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
 	opts = {
 		completions = { blink = { enabled = true } },
 		render_modes = true, -- Show formatted text even while in insert mode
 		file_types = { "markdown", "gitcommit" },
 
+		heading = {
+			border = true,
+			border_virtual = true,
+			position = "inline",
+			left_pad = 2,
+		},
+		code = {
+			width = "block",
+			left_pad = 2,
+			right_pad = 2,
+			language_pad = 2,
+			min_width = 45,
+		},
 		pipe_table = {
 			preset = "round",
+			border_virtual = true,
 		},
-
-		-- Allows word-wrapping plugins like `wrapping.nvim` to work.
+		-- quote = {
+		-- 	repeat_linebreak = true,
+		-- },
+		-- Allows word-wrapping to work in markdown
 		win_options = {
 			wrap = { default = true, rendered = true },
+			breakindent = { default = true, rendered = true },
+			breakindentopt = { default = "list:-1", rendered = "list:-1" },
+			linebreak = { default = true, rendered = true },
 		},
 	},
 }
