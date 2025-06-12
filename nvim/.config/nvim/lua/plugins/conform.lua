ae08dfbe7c6aa62d53df04b26699141a9520989f
Jordan King <kingjordan2718@gmail.com>
2025-06-11 21:27:35 -0700

    refactor(nvim): Reconfigured formatting with eslint and prettier.
    Previously, in certain projects where style rules were being enforced by
    `eslint` rather than `prettier`, I noticed that `prettier`'s formatting
    would conflict with what was dictated by `eslint`. For instance, I was
    having an issue where `eslint` would remove semicolons while `prettier`
    would add them back.
    
    What made the issue worse was that `eslint` would make my screen bleed
    because of the sheer volume of diagnostic errors caused by `prettier`'s
    formatting. This made me abandon `prettier` for awhile, even though I
    quite enjoyed the way it formatted code.
    
    To reconcile this issue, I reconfigured `conform.nvim` to *first* format
    with `prettier`, *then* with `eslint`. This made it so that I
    could keep many of `prettier`'s sensible defaults while still adhering
    to the `eslint` rules defined in my project.
    
    I was only able to accomplish this because of this recipe I found on
    `conform`'s GitHub page: https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#run-the-first-available-formatter-followed-by-more-formatters

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,9 +1,55 @@
+--- Runs the first available formatter given as an argument.
+--- You can use this function to run one formatter first *then* another one.
+---
+--- This code can be found on the [GitHub page](https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#run-the-first-available-formatter-followed-by-more-formatters) for `conform.nvim`.
+---@param bufnr integer The buffer this formatting will be applied to.
+---@param ... string The formatters you want to attempt to run.
+---@return string
+local function first(bufnr, ...)
+	local conform = require("conform")
+	for i = 1, select("#", ...) do
+		local formatter = select(i, ...)
+		if conform.get_formatter_info(formatter, bufnr).available then
+			return formatter
+		end
+	end
+	return select(1, ...)
+end
+
+--- Attempts to format the given buffer with `prettier` *first*, then formats with `eslint_d`.
+---
+--- This is useful for projects where code styles are enforced through `eslint` rules rather than in a `.prettierrc` file, but you still want to apply `prettier`'s clean formatting wherever possible.
+---@param bufnr integer
+local function format_with_prettier_then_eslint(bufnr)
+	return { first(bufnr, "prettierd", "prettier"), "eslint_d" }
+end
+
 -- Formatter plugin (with "format on save")
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = format_with_prettier_then_eslint,
+			typescript = format_with_prettier_then_eslint,
+			typescriptreact = format_with_prettier_then_eslint,
+			markdown = function(bufnr)
+				return { first(bufnr, "prettierd", "prettier"), "injected" }
+			end,
+		},
 
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+
+		format_on_save = {
+			lsp_format = "fallback",
+			timeout_ms = 500,
+		},
+	},
 	keys = {
 		{
 			"<leader>f",
@@ -13,36 +59,4 @@ return {
 			desc = "Format buffer",
 		},
 	},
-
-	config = function()
-		-- Initializes ESLint as the formatter if an ESLint config file is present in the cwd.
-		-- Otherwise, Prettier is used.
-		local eslintWithPrettierFallback = function(bufnr)
-			if require("conform").get_formatter_info("eslint_d", bufnr).available then
-				return { "eslint_d" }
-			else
-				return { "prettierd", "prettier", stop_after_first = true }
-			end
-		end
-
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "isort", "black" },
-				javascript = eslintWithPrettierFallback,
-				typescript = eslintWithPrettierFallback,
-				typescriptreact = { "eslint_d", "prettierd", "prettier", stop_after_first = true },
-				markdown = { "prettierd", "prettier", stop_after_first = true },
-			},
-
-			default_format_opts = {
-				lsp_format = "fallback",
-			},
-
-			format_on_save = {
-				lsp_format = "fallback",
-				timeout_ms = 500,
-			},
-		})
-	end,
 }
