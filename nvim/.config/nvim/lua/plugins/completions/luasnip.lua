0fe666622c0b8a6586748ef4615f34d1039dd336
Jordan King <kingjordan2718@gmail.com>
2025-06-12 16:55:48 -0700

    fix(nvim): Luansip error message that would appear on startup

--- nvim/.config/nvim/lua/plugins/completions/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/completions/luasnip.lua
@@ -6,31 +6,34 @@
 return {
 	"L3MON4D3/LuaSnip",
 	version = "v2.*",
-	dependencies = { "rafamadriz/friendly-snippets" },
+	dependencies = { "rafamadriz/friendly-snippets", "nvim-lua/plenary.nvim" },
 	config = function()
 		-- Load snippets from `friendly-snippets`
 		require("luasnip.loaders.from_vscode").lazy_load()
 
 		-- Apply snippets from all `.vscode/*.code-snippets` files in the cwd.
 		-- Code sourced from https://github.com/erlangparasu/dot-config-nvim/blob/6146f6edbd99225c7d74c12b1e769ad4ebd6c6a9/lua/config/autocmds.lua#L55
-		-- local plenary = require("plenary.scandir")
-		-- local cwd = vim.fn.getcwd()
-		--
-		-- local files = plenary.scan_dir(cwd .. "/.vscode", {
-		-- 	depth = 2,
-		-- 	hidden = true,
-		-- 	search_pattern = ".code[-]snippets$", -- extension ".code-snippets"
-		-- })
-		--
-		-- if #files > 0 then
-		-- 	for _, file in ipairs(files) do
-		-- 		local text1 = file
-		-- 		local substring1 = ".code-snippets"
-		--
-		-- 		if string.find(text1, substring1, 0, true) ~= nil then
-		-- 			require("luasnip.loaders.from_vscode").load_standalone({ lazy = true, path = file })
-		-- 		end
-		-- 	end
-		-- end
+		local plenary = require("plenary.scandir")
+		local cwd = vim.fn.getcwd()
+
+		local files = plenary.scan_dir(cwd .. "/.vscode", {
+			depth = 2,
+			hidden = true,
+			silent = true, -- Prevents error message from echoing when "/.vscode" isn't found
+			search_pattern = ".code[-]snippets$", -- extension ".code-snippets"
+		})
+
+		if #files > 0 then
+			for _, file in ipairs(files) do
+				local text1 = file
+				local substring1 = ".code-snippets"
+
+				if string.find(text1, substring1, 0, true) ~= nil then
+					require("luasnip.loaders.from_vscode").load_standalone({ lazy = true, path = file })
+					vim.cmd.echo(string.find(text1, substring1, 0, true))
+					print(string.find(text1, substring1, 0, true))
+				end
+			end
+		end
 	end,
 }
