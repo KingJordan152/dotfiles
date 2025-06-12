20657d58ec768175853b6ea510f330ee3db8170d
Jordan King <kingjordan2718@gmail.com>
2025-06-12 11:42:09 -0700

    fix(nvim): Fixed `colorizer` initialization
    The `colorizer` plugin now *actually* starts up (lol), and it only does
    so after it's entered a buffer rather than immediately when Neovim
    starts up. This prevents an issue where a `termguicolors` error shows up
    despite that option being set in my config.

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,3 +1,9 @@
 return {
 	"norcalli/nvim-colorizer.lua",
+	-- Allows `termguicolors` to be set before this plugin is activated
+	event = { "BufEnter" },
+	config = function()
+		-- Doesn't work unless I explicitly call setup like this
+		require("colorizer").setup()
+	end,
 }
