446fcfc34f93bd199d4c00e645d17f49b931f2bc
Jordan King <kingjordan2718@gmail.com>
2025-06-25 09:07:55 -0700

    style(nvim): Restyled `lualine` to be more stylish and useful
    I redesigned my statusline via `lualine` so as to provide more
    information and look more visually appealing.
    
    The biggest change I made was adding LSP and `Conform` formatter info to
    the right side of `lualine`, each sporting a custom, colorized icon
    based on the Tokyonight colorscheme.
    
    I also changed icons for all Git modifications, diagnostics, and the
    current file in the buffer.
    
    All of these changes combine to give me a statusline that's extremely
    effective at conveying useful information in an appealing way.

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,27 @@
+local function formatter_status()
+	local conform = require("conform")
+	local formatters_for_current_buffer, lsp_fallback = conform.list_formatters_to_run(0)
+	local icon = "%#LualineFormatterIcon# %#lualine_c_normal#"
+	local result = icon
+
+	-- TODO: Customize appearance based on whether formatter is active.
+	if next(formatters_for_current_buffer) == nil then
+		if lsp_fallback then
+			result = result .. "LSP"
+		else
+			result = ""
+		end
+	else
+		for i, formatter in ipairs(formatters_for_current_buffer) do
+			local separator = i == 1 and "" or ", "
+
+			result = result .. separator .. formatter.name
+		end
+	end
+
+	return result
+end
+
 -- [[
 --    Renders an aesthetic statusbar towards the bottom of the screen containing
 --    information such as my current mode, filename, LSP, etc.
@@ -5,25 +29,63 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			theme = "auto",
-			component_separators = { left = "\\", right = "/" },
-			section_separators = { left = "", right = "" },
-			globalstatus = true,
-		},
-		sections = {
-			lualine_x = {
-				{
-					"lsp_status",
-					icon = " ",
-					symbols = {
-						done = "",
-						separator = "   ",
+	config = function()
+		-- Extend `filename` component to include an icon that corresponds to the filetype.
+		local filename_with_icon = require("lualine.components.filename"):extend()
+		filename_with_icon.apply_icon = require("lualine.components.filetype").apply_icon
+		filename_with_icon.icon_hl_cache = {}
+
+		-- Initialize custom highlight groups.
+		local colors = require("tokyonight.colors").setup()
+		vim.api.nvim_set_hl(0, "LualineLspIcon", { fg = colors.blue1 })
+		vim.api.nvim_set_hl(0, "LualineFormatterIcon", { fg = colors.yellow })
+
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				globalstatus = true,
+			},
+			sections = {
+				lualine_b = {
+					{
+						"branch",
+						draw_empty = true,
+					},
+					{
+						"diff",
+						symbols = { added = " ", modified = " ", removed = " " },
 					},
 				},
-				"filetype",
+				lualine_c = {
+					{ filename_with_icon, colored = true },
+					{
+						"diagnostics",
+						symbols = { error = " ", warn = " ", hint = " ", info = " " },
+					},
+				},
+				lualine_x = {
+
+					{
+						"lsp_status",
+						icon = "%#LualineLspIcon# %#lualine_c_normal#",
+						symbols = {
+							done = "",
+							separator = ", ",
+						},
+						ignore_lsp = { "cssmodules_ls" },
+					},
+					{ formatter_status },
+					"filetype",
+				},
 			},
-		},
-	},
+			extensions = {
+				"quickfix",
+				"lazy",
+				"mason",
+				"man",
+			},
+		})
+	end,
 }
