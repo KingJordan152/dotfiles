6633f92a3ac1982224dca17e8276e3d6f02922f2
Jordan King <kingjordan2718@gmail.com>
2025-06-27 14:57:14 -0700

    feat(nvim)!: Changed how formatting works with Prettier and ESLint
    Previously, I added code to `conform.nvim` that would attempt to run
    `prettierd` on a given TS/JS/TSX/JSX file *first*, then `eslint_d`. I
    did this so that any formatting changes introduced by Prettier that
    didn't comply with ESLint would be fixed by ESLint subsequently.
    
    However, I soon realized that this behavior didn't work reliably with
    `conform`'s `format_on_save` feature. Since `eslint_d` is a node.js
    daemon, it takes a considerable amount of time to cold start after
    Neovim requests it for the first time. So long, in fact, that it would
    exceed `format_on_save`'s `timeout`, which would case Prettier to format
    my file but leave errors that ESLint didn't like.
    
    I initially thought that binding the `eslint_d` process to the runtime
    of Neovim would solve this issue, but that also introduced
    timeout issues.
    
    Unfortunately, after much failed experimentation, the only solution that
    ended up working for me was using `format_after_save` rather than
    `format_on_save`. Since the former is asynchronous, it's impossible to
    get the timeout issues I was experiencing with the latter. Additionally,
    my formatters are always guaranteed to run in the order that I requested
    them to. There will never be a situation where I'll see Prettier-induced
    errors that aren't immediately cleaned up by ESLint.
    
    I'll admit, this solution isn't as snappy as using `format_on_save`, but
    it's the best I've got without further pulling out my hair. In the
    future I might consider doing the following:
    
    - Recreate `eslint_d` using Go or Rust instead of Node (highly ambitious)
    - Conditionally format using `format_on_save` when the filetype isn't
      some JS derivative
    - Add some sort of loading indicator to my statusline so that I know
      when `eslint_d` is taking a long time to format (most likely)
    - Write an article about the torment I've had to endure
    - Fuck everything and just use Biome lol

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -16,26 +16,23 @@ local function first(bufnr, ...)
 	return select(1, ...)
 end
 
---- Attempts to format the given buffer with `prettier` *first*, then formats with `eslint_d`.
----
---- This is useful for projects where code styles are enforced through `eslint` rules rather than in a `.prettierrc` file, but you still want to apply `prettier`'s clean formatting wherever possible.
----@param bufnr integer The buffer this formatting will be applied to.
-local function format_with_prettier_then_eslint(bufnr)
-	return { first(bufnr, "prettierd", "prettier"), "eslint_d" }
-end
-
--- Formatter plugin (with "format on save")
+--[[
+--  Formatter plugin (with "format on save")
+--]]
 return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
+	---@module "conform"
+	---@type conform.setupOpts
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "isort", "black" },
-			javascript = format_with_prettier_then_eslint,
-			typescript = format_with_prettier_then_eslint,
-			typescriptreact = format_with_prettier_then_eslint,
+			css = { "prettierd" },
+			javascript = { "prettierd", "eslint_d" },
+			typescript = { "prettierd", "eslint_d" },
+			typescriptreact = { "prettierd", "eslint_d" },
 			markdown = function(bufnr)
 				return { first(bufnr, "prettierd", "prettier"), "injected" }
 			end,
@@ -45,9 +42,20 @@ return {
 			lsp_format = "fallback",
 		},
 
-		format_on_save = {
-			lsp_format = "fallback",
-			timeout_ms = 500,
+		format_after_save = function(bufnr)
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			return { lsp_format = "fallback" }
+		end,
+
+		formatters = {
+			eslint_d = {
+				env = {
+					ESLINT_D_PPID = vim.fn.getpid(),
+				},
+			},
 		},
 	},
 	init = function()
