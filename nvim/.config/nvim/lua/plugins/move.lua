0d23e221ceed51ae565658c0514d9b77af627d96
Jordan King <kingjordan2718@gmail.com>
2025-07-10 17:06:51 -0700

    fix(nvim): made all `move.nvim` keymaps silent
    I made these keymaps silent becuase it was annoying to
    see `move.nvim` keymaps print `:MoveLine(1)` whenever
    moving a line within the editor.

--- nvim/.config/nvim/lua/plugins/move.lua
+++ nvim/.config/nvim/lua/plugins/move.lua
@@ -2,15 +2,15 @@ return {
 	"fedepujol/move.nvim",
 	keys = {
 		-- Normal Mode
-		{ "<A-j>", ":MoveLine(1)<CR>", desc = "Move Line Up" },
-		{ "<A-k>", ":MoveLine(-1)<CR>", desc = "Move Line Down" },
-		{ "<A-h>", ":MoveHChar(-1)<CR>", desc = "Move Character Left" },
-		{ "<A-l>", ":MoveHChar(1)<CR>", desc = "Move Character Right" },
+		{ "<A-j>", ":MoveLine(1)<CR>", desc = "Move Line Up", silent = true },
+		{ "<A-k>", ":MoveLine(-1)<CR>", desc = "Move Line Down", silent = true },
+		{ "<A-h>", ":MoveHChar(-1)<CR>", desc = "Move Character Left", silent = true },
+		{ "<A-l>", ":MoveHChar(1)<CR>", desc = "Move Character Right", silent = true },
 		-- Visual Mode
-		{ "<A-j>", ":MoveBlock(1)<CR>", mode = { "x" }, desc = "Move Block Up" },
-		{ "<A-k>", ":MoveBlock(-1)<CR>", mode = { "x" }, desc = "Move Block Down" },
-		{ "<A-h>", ":MoveHBlock(-1)<CR>", mode = { "x" }, desc = "Move Block Left" },
-		{ "<A-l>", ":MoveHBlock(1)<CR>", mode = { "x" }, desc = "Move Block Right" },
+		{ "<A-j>", ":MoveBlock(1)<CR>", mode = { "x" }, desc = "Move Block Up", silent = true },
+		{ "<A-k>", ":MoveBlock(-1)<CR>", mode = { "x" }, desc = "Move Block Down", silent = true },
+		{ "<A-h>", ":MoveHBlock(-1)<CR>", mode = { "x" }, desc = "Move Block Left", silent = true },
+		{ "<A-l>", ":MoveHBlock(1)<CR>", mode = { "x" }, desc = "Move Block Right", silent = true },
 	},
 	opts = {
 		char = {
