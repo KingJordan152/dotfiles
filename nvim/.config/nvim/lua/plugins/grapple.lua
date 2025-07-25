e5664b4cbd7925ae5fef71ddb3f6654d6a13b85b
Jordan King <kingjordan2718@gmail.com>
2025-07-25 12:55:00 -0700

    feat(nvim): added keymaps for `grapple`
    I initially used `telescope` for managing tags, but I quickly realized
    that it's not fully featured enough to be useful. So, I switched it out
    for the default one created by the `grapple` maintainer.

--- nvim/.config/nvim/lua/plugins/grapple.lua
+++ nvim/.config/nvim/lua/plugins/grapple.lua
@@ -19,17 +19,17 @@ return {
 	},
 	keys = {
 		-- Recommended keymaps
-		{ "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" }, -- Think of this like an elevated `mark` command
-		{ "<leader>;", "<cmd>Telescope grapple tags<cr>", desc = "Grapple open tags window" },
-		{ "<C-S-N>", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
-		{ "<C-S-P>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
+		{ "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Grapple - Toggle tag" }, -- Think of this like an elevated `mark` command
+		{ "<leader>;", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple - Open tags window" },
+		{ "<C-S-N>", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple - Cycle next tag" },
+		{ "<C-S-P>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple - Cycle previous tag" },
 
 		-- Index-specific keymaps
-		{ "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Select first tag (Grapple)" },
-		{ "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Select second tag (Grapple)" },
-		{ "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Select third tag (Grapple)" },
-		{ "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag (Grapple)" },
-		{ "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Select fifth tag (Grapple)" },
+		{ "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Grapple - Select first tag" },
+		{ "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Grapple - Select second tag" },
+		{ "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple - Select third tag" },
+		{ "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple - Select fourth tag" },
+		{ "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Grapple - Select fifth tag" },
 
 		-- [[
 		-- I initially wanted to have index-specific keymaps mapped to Ctrl+7-0 so that they would function similarly to <C-6> and
