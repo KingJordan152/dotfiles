f297c583f82de6a8aabadceff852258d3a21bec6
Jordan King <kingjordan2718@gmail.com>
2025-09-05 08:18:05 -0700

    perf(nvim): made all debugging plugins lazy-loaded
    I plan on coming back to these plugins and integrating them more cleanly
    into my current setup (they're a hold over from my experimental phase),
    but for now I'm just going to arbitrarily lazy load them in order to
    gain some more performance.

--- nvim/.config/nvim/lua/plugins/debugging/nvim-dap-ui.lua
+++ nvim/.config/nvim/lua/plugins/debugging/nvim-dap-ui.lua
@@ -1,26 +1,26 @@
 return {
-  "rcarriga/nvim-dap-ui",
-  dependencies = {
-    "mfussenegger/nvim-dap",
-    "nvim-neotest/nvim-nio",
-  },
+	"rcarriga/nvim-dap-ui",
+	dependencies = {
+		"mfussenegger/nvim-dap",
+		"nvim-neotest/nvim-nio",
+	},
+	lazy = true,
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
 
-  config = function()
-    local dap, dapui = require("dap"), require("dapui")
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
-
-    require("dapui").setup({})
-  end,
+		require("dapui").setup({})
+	end,
 }
--- nvim/.config/nvim/lua/plugins/debugging/nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/debugging/nvim-dap.lua
@@ -1,30 +1,30 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "leoluz/nvim-dap-go",
-  },
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"leoluz/nvim-dap-go",
+	},
+	lazy = true,
+	config = function()
+		local dap = require("dap")
 
-  config = function()
-    local dap = require("dap")
-
-    vim.keymap.set("n", "<F5>", function()
-      dap.continue()
-    end, { desc = "Debugger: Continue" })
-    vim.keymap.set("n", "<F10>", function()
-      dap.step_over()
-    end, { desc = "Debugger: Step Over" })
-    vim.keymap.set("n", "<F11>", function()
-      dap.step_into()
-    end, { desc = "Debugger: Step Into" })
-    vim.keymap.set("n", "<F12>", function()
-      dap.step_out()
-    end, { desc = "Debugger: Step Out" })
-    vim.keymap.set("n", "<Leader>b", function()
-      dap.toggle_breakpoint()
-    end, { desc = "Debugger: Toggle Breakpoint" })
-    vim.keymap.set("n", "<Leader>lp", function()
-      dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-    end, { desc = "Debugger: Add Log Point" })
-  end,
+		vim.keymap.set("n", "<F5>", function()
+			dap.continue()
+		end, { desc = "Debugger: Continue" })
+		vim.keymap.set("n", "<F10>", function()
+			dap.step_over()
+		end, { desc = "Debugger: Step Over" })
+		vim.keymap.set("n", "<F11>", function()
+			dap.step_into()
+		end, { desc = "Debugger: Step Into" })
+		vim.keymap.set("n", "<F12>", function()
+			dap.step_out()
+		end, { desc = "Debugger: Step Out" })
+		vim.keymap.set("n", "<Leader>b", function()
+			dap.toggle_breakpoint()
+		end, { desc = "Debugger: Toggle Breakpoint" })
+		vim.keymap.set("n", "<Leader>lp", function()
+			dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+		end, { desc = "Debugger: Add Log Point" })
+	end,
 }
