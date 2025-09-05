82bfac5237039e1262472d90960906dad40bcd24
Jordan King <kingjordan2718@gmail.com>
2025-09-05 08:34:45 -0700

    fix(nvim): removed deleted keymaps inside LuaSnip
    Since I've resolved the whole "Tab snippet jump" issue, the keymaps I
    previously deleted inside LuaSnip are no longer necessary (and are
    actually harmful).

--- nvim/.config/nvim/lua/plugins/completions/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/completions/luasnip.lua
@@ -33,9 +33,5 @@ return {
 				end
 			end
 		end
-
-		-- Remove default snippet jump commands because they usually activate at the wrong time
-		vim.keymap.del("i", "<Tab>")
-		vim.keymap.del("i", "<S-Tab>")
 	end,
 }
