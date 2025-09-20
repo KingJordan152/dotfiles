5c452e30e506f34b826738c861a750815c7e205c
Jordan King <kingjordan2718@gmail.com>
2025-09-20 16:33:48 -0700

    refactor(nvim): move vim.o.sessionoptions into `auto-session`
    Moves the `vim.o.sessionoptions` setting into the `auto-session` plugin.
    This plugin recommends setting this option so that it works predictably,
    so it makes sense for it to be placed within its configuration file
    rather than the `option` file.

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -9,4 +9,8 @@ return {
 		args_allow_single_directory = false, -- Allows netrw to be opened using `nvim .`
 		bypass_save_filetypes = { "netrw" },
 	},
+	init = function()
+		-- Specific option needed by AutoSession to properly save everything.
+		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+	end,
 }
