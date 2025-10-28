6772d83ee3b1766104e9d5fccac142c3eae7539e
Jordan King <kingjordan2718@gmail.com>
2025-10-27 17:22:32 -0700

    docs(nvim): change Grapple index keymap desc to be more readable/correct

--- nvim/.config/nvim/lua/plugins/grapple.lua
+++ nvim/.config/nvim/lua/plugins/grapple.lua
@@ -25,14 +25,14 @@ return {
 		{ "<C-S-P>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple - Cycle previous tag" },
 
 		-- Index-specific keymaps
-		{ "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Grapple - Select first tag" },
-		{ "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Grapple - Select second tag" },
-		{ "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple - Select third tag" },
-		{ "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple - Select fourth tag" },
-		{ "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Grapple - Select fifth tag" },
-		{ "<leader>6", "<cmd>Grapple select index=6<cr>", desc = "Grapple - Select fifth tag" },
-		{ "<leader>7", "<cmd>Grapple select index=7<cr>", desc = "Grapple - Select fifth tag" },
-		{ "<leader>8", "<cmd>Grapple select index=8<cr>", desc = "Grapple - Select fifth tag" },
-		{ "<leader>9", "<cmd>Grapple select index=9<cr>", desc = "Grapple - Select fifth tag" },
+		{ "<leader>1", "<cmd>Grapple select index=1<cr>", desc = "Grapple - Select tag 1" },
+		{ "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Grapple - Select tag 2" },
+		{ "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple - Select tag 3" },
+		{ "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple - Select tag 4" },
+		{ "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Grapple - Select tag 5" },
+		{ "<leader>6", "<cmd>Grapple select index=6<cr>", desc = "Grapple - Select tag 6" },
+		{ "<leader>7", "<cmd>Grapple select index=7<cr>", desc = "Grapple - Select tag 7" },
+		{ "<leader>8", "<cmd>Grapple select index=8<cr>", desc = "Grapple - Select tag 8" },
+		{ "<leader>9", "<cmd>Grapple select index=9<cr>", desc = "Grapple - Select tag 9" },
 	},
 }
