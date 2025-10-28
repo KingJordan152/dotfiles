9f746405a756a450381201ea1be79a9152d91d47
Jordan King <kingjordan2718@gmail.com>
2025-10-27 17:20:51 -0700

    fix(nvim): make Grapple index keymaps above 5 work correctly

--- nvim/.config/nvim/lua/plugins/grapple.lua
+++ nvim/.config/nvim/lua/plugins/grapple.lua
@@ -30,9 +30,9 @@ return {
 		{ "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Grapple - Select third tag" },
 		{ "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Grapple - Select fourth tag" },
 		{ "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Grapple - Select fifth tag" },
-		{ "<leader>6", "<cmd>Grapple select index=5<cr>", desc = "Grapple - Select fifth tag" },
-		{ "<leader>7", "<cmd>Grapple select index=5<cr>", desc = "Grapple - Select fifth tag" },
-		{ "<leader>8", "<cmd>Grapple select index=5<cr>", desc = "Grapple - Select fifth tag" },
-		{ "<leader>9", "<cmd>Grapple select index=5<cr>", desc = "Grapple - Select fifth tag" },
+		{ "<leader>6", "<cmd>Grapple select index=6<cr>", desc = "Grapple - Select fifth tag" },
+		{ "<leader>7", "<cmd>Grapple select index=7<cr>", desc = "Grapple - Select fifth tag" },
+		{ "<leader>8", "<cmd>Grapple select index=8<cr>", desc = "Grapple - Select fifth tag" },
+		{ "<leader>9", "<cmd>Grapple select index=9<cr>", desc = "Grapple - Select fifth tag" },
 	},
 }
