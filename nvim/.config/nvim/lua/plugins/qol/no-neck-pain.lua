c33c20b9fea3724c61f94aac3a0224a0157330fd
Jordan King <kingjordan2718@gmail.com>
2025-10-30 18:35:27 -0700

    feat(nvim): make `No-Neck-Pain` act more like zen mode in markdown

--- nvim/.config/nvim/lua/plugins/qol/no-neck-pain.lua
+++ nvim/.config/nvim/lua/plugins/qol/no-neck-pain.lua
@@ -1,14 +1,32 @@
 return {
 	"shortcuts/no-neck-pain.nvim",
 	version = "*",
-	opts = {
-		width = 110,
-	},
+	cmd = "NoNeckPain",
 	keys = {
 		{
 			"<leader>zz",
-			"<Cmd>NoNeckPain<CR>",
-			desc = "Center window",
+			"<cmd>:NoNeckPain<cr>",
+			desc = "Center current window",
 		},
 	},
+	opts = function()
+		require("no-neck-pain").setup({
+			width = 120,
+			disableOnLastBuffer = true,
+			fallbackOnBufferDelete = false,
+		})
+
+		vim.api.nvim_create_autocmd("BufEnter", {
+			desc = "Remove vertical split lines in markdown files when NoNeckPain is enabled",
+			group = vim.api.nvim_create_augroup("markdown_remove_vertical_split_lines", { clear = true }),
+			callback = function()
+				local filetype = vim.bo.filetype
+				if filetype == "markdown" or filetype == "no-neck-pain" then
+					vim.opt.fillchars:append({ vert = " " })
+				else
+					vim.opt.fillchars:append({ vert = "│" })
+				end
+			end,
+		})
+	end,
 }
