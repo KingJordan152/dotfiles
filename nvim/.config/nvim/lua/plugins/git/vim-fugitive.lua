4b535dffe775719d584319a00e5af4de9a0b3b99
Jordan King <kingjordan2718@gmail.com>
2025-11-21 10:38:03 -0800

    feat(nvim): make custom Fugitive keymap easier to use
    Changes custom Fugitive keymap from `<leader>G` to `<leader>gg`, the
    latter of which being easier to do.

--- nvim/.config/nvim/lua/plugins/git/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/git/vim-fugitive.lua
@@ -1,6 +1,6 @@
 return {
 	"tpope/vim-fugitive",
 	init = function()
-		vim.keymap.set("n", "<leader>G", "<Cmd>G<CR>", { desc = "Open Fugitive summary window" })
+		vim.keymap.set("n", "<leader>gg", "<Cmd>G<CR>", { desc = "Fugitive Summary Window" })
 	end,
 }
