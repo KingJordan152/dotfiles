531bd2000a4ec326511f1f7736ce3a802746dd88
Jordan King <kingjordan2718@gmail.com>
2025-12-01 12:15:33 -0800

    feat(nvim): add type definitions to `render-markdown` opts

--- nvim/.config/nvim/lua/plugins/writing/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/writing/render-markdown.lua
@@ -2,6 +2,8 @@ return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	ft = { "markdown", "gitcommit" },
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
 	opts = {
 		completions = { lsp = { enabled = true } },
 		render_modes = true, -- Show formatted text even while in insert mode
