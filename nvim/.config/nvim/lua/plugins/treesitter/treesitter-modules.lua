67fb680aaf9c29d2d388dd29e6d4f388de514d0e
Jordan King <kingjordan2718@gmail.com>
2025-11-30 22:18:09 -0800

    refactor(nvim): separate `treesitter` and `treesitter-modules`

---
+++ nvim/.config/nvim/lua/plugins/treesitter/treesitter-modules.lua
@@ -0,0 +1,39 @@
+return {
+	"MeanderingProgrammer/treesitter-modules.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
+	---@module 'treesitter-modules'
+	---@type ts.mod.UserConfig
+	opts = {
+		auto_install = true,
+		ensure_installed = {
+			"lua",
+			"luadoc",
+			"tsx",
+			"typescript",
+			"javascript",
+			"jsdoc",
+			"html",
+			"css",
+			"styled", -- For styled-components
+			"markdown",
+			"markdown_inline",
+			"yaml",
+			"regex", -- For certain pickers
+			"latex",
+			"query",
+			"gitcommit",
+			"c",
+		},
+		highlight = {
+			enable = true,
+		},
+		fold = {
+			enable = true,
+		},
+		indent = {
+			enable = true,
+		},
+	},
+}
--- nvim/.config/nvim/lua/plugins/treesitter/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter/treesitter.lua
@@ -1,41 +1,6 @@
 return {
-	"MeanderingProgrammer/treesitter-modules.nvim",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		branch = "main",
-		build = ":TSUpdate",
-	},
-	---@module 'treesitter-modules'
-	---@type ts.mod.UserConfig
-	opts = {
-		auto_install = true,
-		ensure_installed = {
-			"lua",
-			"luadoc",
-			"tsx",
-			"typescript",
-			"javascript",
-			"jsdoc",
-			"html",
-			"css",
-			"styled", -- For styled-components
-			"markdown",
-			"markdown_inline",
-			"yaml",
-			"regex", -- For certain pickers
-			"latex",
-			"query",
-			"gitcommit",
-			"c",
-		},
-		highlight = {
-			enable = true,
-		},
-		fold = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-	},
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	branch = "main",
+	build = ":TSUpdate",
 }
