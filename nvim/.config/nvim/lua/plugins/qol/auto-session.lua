1ad7441c59420565c6ba73847fa05ce27e5b3bb0
Jordan King <kingjordan2718@gmail.com>
2025-12-11 16:29:34 -0800

    feat(nvim): save auto-formatting state via `AutoSession`
    Now, the status of auto-formatting, both globally
    and buffer-locally, is saved to the session created by `AutoSession`.
    
    This makes it so that the auto-formatting state is persisted when
    closing and reopening Neovim.

--- nvim/.config/nvim/lua/plugins/qol/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/qol/auto-session.lua
@@ -6,8 +6,26 @@ return {
 	---@type AutoSession.Config
 	opts = {
 		suppressed_dirs = { "~/", "/", "~/Downloads" },
-		args_allow_single_directory = false, -- Allows netrw to be opened using `nvim .`
-		bypass_save_filetypes = { "netrw" },
+		args_allow_single_directory = false, -- Allows Netrw/Oil to be opened using `nvim .`
+		bypass_save_filetypes = { "netrw", "snacks_dashboard" },
+
+		save_extra_data = function()
+			local extra_data = {
+				autoformatting = {
+					global = vim.g.disable_autoformat,
+					buffer = vim.b.disable_autoformat,
+				},
+			}
+
+			return vim.fn.json_encode(extra_data)
+		end,
+
+		restore_extra_data = function(_, json_data)
+			local extra_data = vim.fn.json_decode(json_data)
+
+			vim.g.disable_autoformat = extra_data.autoformatting.global
+			vim.b.disable_autoformat = extra_data.autoformatting.buffer
+		end,
 	},
 	init = function()
 		-- Specific option needed by AutoSession to properly save everything.
