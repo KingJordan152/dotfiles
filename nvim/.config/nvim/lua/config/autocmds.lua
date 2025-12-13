13bce7e80b6981d3e56015421755f1641ab35a63
Jordan King <kingjordan2718@gmail.com>
2025-12-12 16:22:47 -0800

    feat(nvim): disable diagnostic virtual text for writing-based filetypes
    Add an `autocmd` that disables diagnostic virtual text for the following
    filetypes:
    
    - markdown
    - text
    - gitcommit
    
    This makes the diagnostics for these files behave similarly to common
    word processors, where only undercurls are shown for grammatical errors
    and spelling mistakes.
    
    Additionally, this makes diagnostics less obtrusive when using plugins
    like `no-neck-pain` and writing prose or documentation.

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -42,3 +42,25 @@ autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+local virtual_text_disabled_filetypes = utils.Set({
+	"markdown",
+	"text",
+	"gitcommit",
+})
+
+autocmd("LspAttach", {
+	desc = "Disable diagnostic virtual text for specific filetypes",
+	group = augroup("disable_virtual_text", { clear = true }),
+	callback = function(args)
+		local buf_ft = vim.bo[args.buf].filetype
+
+		if virtual_text_disabled_filetypes[buf_ft] then
+			local ns = vim.lsp.diagnostic.get_namespace(args.data.client_id)
+
+			vim.diagnostic.config({
+				virtual_text = false,
+			}, ns)
+		end
+	end,
+})
