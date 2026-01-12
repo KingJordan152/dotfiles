dbed6a3189eb26d4b271da0b6dd739ffe5e67adf
Jordan King <kingjordan2718@gmail.com>
2026-01-12 12:37:39 -0800

    feat(nvim): add keymap for staging all changes in a project

--- nvim/.config/nvim/lua/plugins/git/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/git/vim-fugitive.lua
@@ -2,5 +2,6 @@ return {
 	"tpope/vim-fugitive",
 	init = function()
 		vim.keymap.set("n", "<leader>gg", "<Cmd>G<CR>", { desc = "Fugitive Summary Window" })
+		vim.keymap.set("n", "<leader>ga.", "<cmd>G add .<cr>", { desc = "All changes" })
 	end,
 }
