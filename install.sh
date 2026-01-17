6e15733613dfc121819bbaf2b0bafea7edf7d813
Jordan King <kingjordan2718@gmail.com>
2026-01-16 22:18:24 -0800

    feat: add `install.sh` script for DevPod usage
    I'm currently experimenting with installing my Dotfiles inside a
    devcontainer via DevPod.
    
    If this process works out and ends up being highly reproducible, expect
    this install script to become more robust over time.

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+packages=(
+  fd
+  ripgrep
+)
+
+modules=(
+  nvim
+  tmux
+)
+
+for package in "${packages[@]}"; do
+  echo "Installing $package..."
+  /home/linuxbrew/.linuxbrew/brew install "$package"
+done
+
+for module in "${modules[@]}"; do
+  echo "Stowing $module..."
+  stow "$module"
+done
+
+echo "Dotfiles successfully installed!"
