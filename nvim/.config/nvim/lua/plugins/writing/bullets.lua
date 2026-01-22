2a3ac9fc2312bb3e3a7bd2098d00d453e2c3969f
Jordan King <kingjordan2718@gmail.com>
2026-01-22 10:29:00 -0800

    feat(nvim): add more keymaps to `bullets-vim`
    This commit adds new keymaps for promoting and demoting keymaps via
    `bullets-vim`. They resemble similar keymaps you would use in a Markdown
    editor like Obsidian.
    
    - `<Tab>` -> demote bullet
    - `<S-Tab>` -> promote bullet

--- nvim/.config/nvim/lua/plugins/writing/bullets.lua
+++ nvim/.config/nvim/lua/plugins/writing/bullets.lua
@@ -3,5 +3,26 @@ return {
 	ft = { "markdown", "gitcommit" },
 	init = function()
 		vim.g.bullets_outline_levels = { "ROM", "ABC", "num", "abc", "rom", "std-" }
+		vim.g.bullets_delete_last_bullet_if_empty = 2
+
+		vim.g.bullets_custom_mappings = {
+			{ "imap", "<cr>", "<Plug>(bullets-newline)" },
+			{ "nmap", "o", "<Plug>(bullets-newline)" },
+			{ "inoremap", "<C-cr>", "<cr>" },
+
+			{ "vmap", "gN", "<Plug>(bullets-renumber)" },
+			{ "nmap", "gN", "<Plug>(bullets-renumber)" },
+
+			{ "nmap", "gX", "<Plug>(bullets-toggle-checkbox)" },
+
+			{ "imap", "<C-t>", "<Plug>(bullets-demote)" },
+			{ "imap", "<Tab>", "<Plug>(bullets-demote)" },
+			{ "nmap", ">>", "<Plug>(bullets-demote)" },
+			{ "vmap", ">", "<Plug>(bullets-demote)" },
+			{ "imap", "<C-d>", "<Plug>(bullets-promote)" },
+			{ "imap", "<S-Tab>", "<Plug>(bullets-promote)" },
+			{ "nmap", "<<", "<Plug>(bullets-promote)" },
+			{ "vmap", "<", "<Plug>(bullets-promote)" },
+		}
 	end,
 }
