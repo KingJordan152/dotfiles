9ef9ca31282be7236e9c03d67cae11b610da72b3
Jordan King <kingjordan2718@gmail.com>
2026-01-26 19:37:27 -0800

    feat(nvim): add LSP config for `vtsls`
    Adds full configuration for `vtsls`.
    
    Awhile back, I tried to use `vtsls` instead of `ts_ls`, but I kept
    encountering autocompletion latency that made it practically unusable.
    
    Fortunately, that issue doesn't seem to be present anymore. `vtsls`
    feels just as fast as `ts_ls`, but since it's a wrapper over VS Code's
    TypeScript LSP, it feels more feature complete.
    
    For instance, one neat aspect of the config that I'm proud of is the
    'SelectTypeScriptVersion' user command that I made. It enables the user
    to select their desired TypeScript version in the exact same way that VS
    Code would. Although I don't know how to persist the user's choice yet,
    it's still an extremely useful and neat tool to have.
    
    At any rate, `vtsls` is setup to use the workspace TypeScript version
    anyways, which is what I typically gravitate towards in any project
    anyways.
    
    It's also configured to work seamlessly with Svelte, Vue, and Styled
    Components, similar to `ts_ls`.
    
    I will try using `vtsls` for a while to see if I truly like it. Or at
    least until `tsgo` finally adds support for semantic tokens.
    
    I will also leave the `ts_ls` config here so I can easily compare the
    two.

---
+++ nvim/.config/nvim/after/lsp/vtsls.lua
@@ -0,0 +1,71 @@
+local nvm_path = vim.fn.expand("$NVM_BIN/../lib")
+
+local vue_language_server_path = vim.fn.expand("$MASON/packages")
+	.. "/vue-language-server"
+	.. "/node_modules/@vue/language-server"
+
+local svelte_language_server_path = vim.fn.expand("$MASON/packages")
+	.. "/svelte-language-server"
+	.. "/node_modules/typescript-svelte-plugin"
+
+local styled_components_plugin_path = (vim.fn.isdirectory(nvm_path) and nvm_path or "/usr/local/lib") .. "/node_modules"
+
+local vue_plugin = {
+	name = "@vue/typescript-plugin",
+	location = vue_language_server_path,
+	languages = { "vue" },
+	configNamespace = "typescript",
+	enableForWorkspaceTypeScriptVersions = true,
+}
+
+local svelte_plugin = {
+	name = "typescript-svelte-plugin",
+	location = svelte_language_server_path,
+	enableForWorkspaceTypeScriptVersions = true,
+}
+
+local styled_components_plugin = {
+	name = "@styled/typescript-styled-plugin",
+	location = styled_components_plugin_path,
+	enableForWorkspaceTypeScriptVersions = true,
+}
+
+return {
+	filetypes = {
+		"typescript",
+		"javascript",
+		"javascriptreact",
+		"typescriptreact",
+		"vue",
+	},
+	settings = {
+		vtsls = {
+			autoUseWorkspaceTsdk = vim.bo.filetype ~= "vue", -- Currently a bug with Vue's default workspace TypeScript version
+			tsserver = {
+				maxTsServerMemory = 8092, -- 8 GiB
+				globalPlugins = {
+					vue_plugin,
+					svelte_plugin,
+					styled_components_plugin,
+				},
+			},
+			experimental = {
+				completion = {
+					enableServerSideFuzzyMatch = true,
+				},
+			},
+		},
+		typescript = {
+			preferences = {
+				preferTypeOnlyAutoImports = true,
+			},
+		},
+	},
+	on_attach = function(client, bufnr)
+		vim.api.nvim_buf_create_user_command(bufnr, "SelectTypeScriptVersion", function()
+			client:request_sync("workspace/executeCommand", {
+				command = "typescript.selectTypeScriptVersion",
+			}, nil, bufnr)
+		end, { desc = "Prompt the user for their preferred TypeScript version" })
+	end,
+}
