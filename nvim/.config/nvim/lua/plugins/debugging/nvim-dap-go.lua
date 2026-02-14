64e19a7811d1763e15c315c877e5a6c1fd0325cb
Jordan King <kingjordan2718@gmail.com>
2026-02-13 21:31:48 -0800

    refactor(nvim): make Go debugging experience more robust
    Ensure "delve" debug adapter is installed, and create keymaps for
    different functions provided by `nvim-dap-go`.

--- nvim/.config/nvim/lua/plugins/debugging/nvim-dap-go.lua
+++ nvim/.config/nvim/lua/plugins/debugging/nvim-dap-go.lua
@@ -2,6 +2,10 @@ return {
 	"leoluz/nvim-dap-go",
 	ft = "go",
 	config = function()
-		require("dap-go").setup()
+		local dap_go = require("dap-go")
+		dap_go.setup()
+
+		vim.keymap.set("n", "<leader>dt", dap_go.debug_test, { desc = "Go - Debug method closest to cursor" })
+		vim.keymap.set("n", "<leader>dT", dap_go.debug_last_test, { desc = "Go - Debug last test" })
 	end,
 }
