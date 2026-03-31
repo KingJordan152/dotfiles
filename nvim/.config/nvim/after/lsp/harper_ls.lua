7dfaa01fde42c13986801ccaff648f54ae6ad8cc
Jordan King <kingjordan2718@gmail.com>
2026-03-30 19:40:13 -0700

    refactor(nvim): disable `virtual_text` per prose LSP, not globally
    Previously, I had used a global `autocmd` to disable diagnostic
    `virtual_text` for all prose-filetypes. However, I've found that it's
    easier to modify the diagnostic config directly from the `on_attach` method of
    all the LSPs I'm using for prose files. For the time being, that's only
    `harper_ls`, but if that changes in the future I'll extract the method
    and add it to any other LSPs.

--- nvim/.config/nvim/after/lsp/harper_ls.lua
+++ nvim/.config/nvim/after/lsp/harper_ls.lua
@@ -4,5 +4,25 @@ return {
 		"markdown",
 		"text",
 		"gitcommit",
+		"typst",
 	},
+	settings = {
+		["harper-ls"] = {
+			diagnosticSeverity = "error",
+			linters = {
+				ToDoHyphen = false, -- Conflicts with `todo-comments`
+				SentenceCapitalization = false, -- Obvious and rarely happens
+
+				-- Undo expansions that conflict with expected technical jargon
+				ExpandArgument = false,
+				ExpandMemoryShorthands = false,
+			},
+		},
+	},
+	on_attach = function(client)
+		vim.diagnostic.config({
+			virtual_text = false,
+			update_in_insert = true,
+		}, vim.lsp.diagnostic.get_namespace(client.id))
+	end,
 }
--- nvim/.config/nvim/lua/config/diagnostics.lua
+++ nvim/.config/nvim/lua/config/diagnostics.lua
@@ -1,9 +1,4 @@
 local utils = require("core.utils")
-local prose_filetypes = utils.Set({
-	"markdown",
-	"text",
-	"gitcommit",
-})
 
 --- Customize how diagnostics work/appear
 vim.diagnostic.config({
@@ -27,21 +22,4 @@ vim.diagnostic.config({
 			[vim.diagnostic.severity.HINT] = " ",
 		},
 	},
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	desc = "Adjust diagnostic config for prose-based filetypes",
-	group = vim.api.nvim_create_augroup("prose_filetype_diagnostics", { clear = true }),
-	callback = function(args)
-		local buf_ft = vim.bo[args.buf].filetype
-
-		if prose_filetypes[buf_ft] then
-			local ns = vim.lsp.diagnostic.get_namespace(args.data.client_id)
-
-			vim.diagnostic.config({
-				virtual_text = false,
-				update_in_insert = true,
-			}, ns)
-		end
-	end,
 })
