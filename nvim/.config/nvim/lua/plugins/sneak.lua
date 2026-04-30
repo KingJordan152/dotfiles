55c33f917dd6586b460a3e0596dd23fdf63d2275
Jordan King <kingjordan2718@gmail.com>
2026-04-29 22:41:49 -0700

    refactor(nvim): rewrite `Sneak` to use `vim.pack`

--- nvim/.config/nvim/lua/plugins/sneak.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"justinmk/vim-sneak",
-	init = function()
-		-- Disable *ugly* highlighting (colorscheme might supply own)
-		vim.cmd([[highlight! link Sneak None]])
-		vim.cmd([[highlight! link SneakCurrent None]])
-	end,
-}
