17dddcc26b929551e80e7d0e917d12ba1cceb62b
Jordan King <kingjordan2718@gmail.com>
2026-05-03 16:19:10 -0700

    refactor(nvim): rewrite `scrollEOF` to use `vim.pack`
    Before doing this, I did a considerable amount of research into whether
    I could adapt my own autocmd for this.
    
    I found that indeed I could, however my implementation did not account
    for folds or convenient performance optimizations like `scrollEOF`, so I
    decided to abandon it.
    
    Hopefully at some point this will just become part of Neovim. It's
    ridiculous that this standard editor behavior needs to be hacked into
    Neovim rather than it just being available from the get-go.

--- nvim/.config/nvim/plugin/scrollEOF.lua
+++ nvim/.config/nvim/plugin/scrollEOF.lua
@@ -1,7 +1,5 @@
-return {
-	"Aasim-A/scrollEOF.nvim",
-	event = { "CursorMoved", "WinScrolled" },
-	opts = {
-		insert_mode = true,
-	},
-}
+vim.pack.add({ "https://github.com/Aasim-A/scrollEOF.nvim" })
+
+require("scrollEOF").setup({
+	insert_mode = true,
+})
