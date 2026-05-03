ba44fbefa4ae3a68563d9535c7083a07a1545ff0
Jordan King <kingjordan2718@gmail.com>
2026-05-03 15:17:22 -0700

    fix(nvim): remove `ignore error` comment from `luasnip`

--- nvim/.config/nvim/plugin/luasnip.lua
+++ nvim/.config/nvim/plugin/luasnip.lua
@@ -11,7 +11,6 @@ local vscode_dir_exists = vim.fn.isdirectory(vscode_dir) == 1
 
 ---@type string[]
 local code_snippet_files = vscode_dir_exists
-		---@diagnostic disable-next-line: param-type-mismatch
 		and vim.fn.readdir(vscode_dir, function(name)
 			return name:match(".code[-]snippets$") ~= nil and 1 or 0 -- `readdir` only accepts numbers in return
 		end)
