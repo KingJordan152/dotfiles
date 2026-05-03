5d61fe99ef11f170c5e4cf637db35ec6caec3740
Jordan King <kingjordan2718@gmail.com>
2026-05-03 15:15:53 -0700

    refactor(nvim): rewrite `luasnip` to use `vim.pack`

--- nvim/.config/nvim/plugin/luasnip.lua
+++ nvim/.config/nvim/plugin/luasnip.lua
@@ -1,35 +1,29 @@
--- [[
--- Plugin for loading and using snippets from various sources.
--- I'm primarily using it so that I can use both `friendly-snippets` and VS Code snippets
--- that are defined in any project that I'm working on.
--- ]]
-return {
-	"L3MON4D3/LuaSnip",
-	version = "v2.*",
-	event = { "InsertEnter", "CmdlineEnter", "TermEnter" },
-	dependencies = { "rafamadriz/friendly-snippets" },
-	config = function()
-		local luasnip_vscode = require("luasnip.loaders.from_vscode")
-		local vscode_dir = vim.fn.getcwd() .. "/.vscode"
-		local vscode_dir_exists = vim.fn.isdirectory(vscode_dir) == 1
+vim.pack.add({
+	-- Dependencies
+	"https://github.com/rafamadriz/friendly-snippets",
 
-		---@type string[]
-		local code_snippet_files = vscode_dir_exists
-				---@diagnostic disable-next-line: param-type-mismatch
-				and vim.fn.readdir(vscode_dir, function(name)
-					return name:match(".code[-]snippets$") ~= nil
-				end)
-			or {}
+	"https://github.com/L3MON4D3/LuaSnip",
+})
 
-		-- Load snippets from `friendly-snippets`
-		luasnip_vscode.lazy_load()
+local luasnip_vscode = require("luasnip.loaders.from_vscode")
+local vscode_dir = vim.fn.getcwd() .. "/.vscode"
+local vscode_dir_exists = vim.fn.isdirectory(vscode_dir) == 1
 
-		-- Apply snippets from all `.vscode/*.code-snippets` files in the cwd, if they exist.
-		-- See https://github.com/L3MON4D3/LuaSnip/issues/1186
-		for _, file in ipairs(code_snippet_files) do
-			local filepath = vscode_dir .. "/" .. file
+---@type string[]
+local code_snippet_files = vscode_dir_exists
+		---@diagnostic disable-next-line: param-type-mismatch
+		and vim.fn.readdir(vscode_dir, function(name)
+			return name:match(".code[-]snippets$") ~= nil and 1 or 0 -- `readdir` only accepts numbers in return
+		end)
+	or {}
 
-			luasnip_vscode.load_standalone({ lazy = true, path = filepath })
-		end
-	end,
-}
+-- Load snippets from `friendly-snippets`
+luasnip_vscode.lazy_load()
+
+-- Apply snippets from all `.vscode/*.code-snippets` files in the cwd, if they exist.
+-- See https://github.com/L3MON4D3/LuaSnip/issues/1186
+for _, file in ipairs(code_snippet_files) do
+	local filepath = vscode_dir .. "/" .. file
+
+	luasnip_vscode.load_standalone({ lazy = true, path = filepath })
+end
