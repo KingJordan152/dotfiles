f97757b7deacac3562b5415977422bb1c9893ddb
Jordan King <kingjordan2718@gmail.com>
2026-05-03 15:10:45 -0700

    refactor(nvim): rewrite `rainbow-delimiters` to use `vim.pack`

--- nvim/.config/nvim/plugin/01-rainbow-delimiters.lua
+++ nvim/.config/nvim/plugin/01-rainbow-delimiters.lua
@@ -1,22 +1,19 @@
-return {
-	"HiPhish/rainbow-delimiters.nvim",
-	config = function()
-		---Links out to a custom script that will strip all HTML tag-based highlighting
-		local remove_tag_delimiters = "rainbow-script"
+vim.pack.add({ "https://github.com/HiPhish/rainbow-delimiters.nvim" })
 
-		require("rainbow-delimiters.setup").setup({
-			-- Highlights come from `indent-blankline`
+---Links out to a custom script that will strip all HTML tag-based highlighting
+local remove_tag_delimiters = "rainbow-script"
 
-			-- Don't colorize corresponding JSX/HTML tags
-			query = {
-				javascript = "rainbow-parens",
-				tsx = "rainbow-parens",
-				typescript = "rainbow-parens",
-				html = remove_tag_delimiters,
-				astro = remove_tag_delimiters,
-				vue = remove_tag_delimiters,
-				svelte = remove_tag_delimiters,
-			},
-		})
-	end,
-}
+require("rainbow-delimiters.setup").setup({
+	-- Highlights come from `indent-blankline`
+
+	-- Don't colorize corresponding JSX/HTML tags
+	query = {
+		javascript = "rainbow-parens",
+		tsx = "rainbow-parens",
+		typescript = "rainbow-parens",
+		html = remove_tag_delimiters,
+		astro = remove_tag_delimiters,
+		vue = remove_tag_delimiters,
+		svelte = remove_tag_delimiters,
+	},
+})
