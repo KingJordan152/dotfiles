be6387dac64e3eb27c32c8282884457117ce6a21
Jordan King <kingjordan2718@gmail.com>
2026-05-02 22:30:22 -0700

    refactor(nvim): rewrite `fugitive` to use `vim.pack`
    I also removed the `<leader>ga.` keymap. Not only did I not use it, but
    it's also rather destructive. Staging all changes should really be left
    to the command-line.

--- nvim/.config/nvim/plugin/vim-fugitive.lua
+++ nvim/.config/nvim/plugin/vim-fugitive.lua
@@ -1,7 +1,3 @@
-return {
-	"tpope/vim-fugitive",
-	init = function()
-		vim.keymap.set("n", "<leader>gg", "<Cmd>G<CR>", { desc = "Fugitive Summary Window" })
-		vim.keymap.set("n", "<leader>ga.", "<cmd>G add .<cr>", { desc = "All changes" })
-	end,
-}
+vim.pack.add({ "https://github.com/tpope/vim-fugitive" })
+
+vim.keymap.set("n", "<leader>gg", "<Cmd>G<CR>", { desc = "Fugitive summary window" })
