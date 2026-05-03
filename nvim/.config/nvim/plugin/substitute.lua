9e4b95d614c3fd7841a14f733a5e23c615a1c2d5
Jordan King <kingjordan2718@gmail.com>
2026-05-03 16:16:30 -0700

    refactor(nvim): rewrite `substitute` to use `vim.pack`
    Also add new keymaps for performing range substitution. This has always
    been built into `substitute`, but I've never understood its purpose
    until now.

--- nvim/.config/nvim/plugin/substitute.lua
+++ nvim/.config/nvim/plugin/substitute.lua
@@ -1,72 +1,41 @@
--- [[
---    Provides commands for substituting and exchanging text over a given motion.
---    This is a more modern version of ReplaceWithRegister.
--- ]]
-return {
-	"gbprod/substitute.nvim",
-	opts = {
-		highlight_substituted_text = {
-			enabled = true,
-			timer = 150, -- Match highlight on yank timeout
-		},
+vim.pack.add({ "https://github.com/gbprod/substitute.nvim" })
+
+local sub = require("substitute")
+local sub_range = require("substitute.range")
+local sub_exchange = require("substitute.exchange")
+
+sub.setup({
+	highlight_substituted_text = {
+		enabled = true,
+		timer = 150, -- Match highlight on yank timeout
 	},
-	keys = {
-		-- Substitute (replace)
-		{
-			"gs",
-			function()
-				require("substitute").operator({
-					modifiers = { "reindent" },
-				})
-			end,
-			desc = "Substitute",
-		},
-		{
-			"gss",
-			function()
-				require("substitute").line({
-					modifiers = { "reindent" },
-				})
-			end,
-			desc = "Substitute line",
-		},
-		{
-			"gS",
-			function()
-				require("substitute").eol()
-			end,
-			desc = "Substitute (up to EOL)",
-		},
-		{
-			"gs",
-			function()
-				require("substitute").visual()
-			end,
-			mode = "x",
-			desc = "Substitute",
-		},
-		-- Exchange (swap values)
-		{
-			"<leader>x",
-			function()
-				require("substitute.exchange").operator()
-			end,
-			desc = "Exchange",
-		},
-		{
-			"<leader>xx",
-			function()
-				require("substitute.exchange").line()
-			end,
-			desc = "Exchange line",
-		},
-		{
-			"<leader>x",
-			function()
-				require("substitute.exchange").visual()
-			end,
-			mode = "x",
-			desc = "Exchange",
-		},
+	range = {
+		confirm = true,
 	},
-}
+})
+
+-- Substitute (replace)
+vim.keymap.set("n", "gs", function()
+	sub.operator({
+		modifiers = { "reindent" },
+	})
+end, { desc = "Substitute" })
+
+vim.keymap.set("n", "gss", function()
+	sub.line({
+		modifiers = { "reindent" },
+	})
+end, { desc = "Substitute line" })
+
+vim.keymap.set("n", "gS", sub.eol, { desc = "Substitute (up to EOL)" })
+vim.keymap.set("x", "gs", sub.visual, { desc = "Substitute" })
+
+-- Range (substitutes a pattern over a specified motion via the command-line)
+vim.keymap.set("n", "<leader>S", sub_range.operator, { desc = "Range substitution" })
+vim.keymap.set("x", "<leader>S", sub_range.visual, { desc = "Range substitution" })
+vim.keymap.set("n", "<leader>SS", sub_range.word, { desc = "Range substitution (word)" })
+
+-- Exchange (swap values)
+vim.keymap.set("n", "<leader>x", sub_exchange.operator, { desc = "Exchange" })
+vim.keymap.set("n", "<leader>xx", sub_exchange.line, { desc = "Exchange line" })
+vim.keymap.set("x", "<leader>xx", sub_exchange.visual, { desc = "Exchange" })
