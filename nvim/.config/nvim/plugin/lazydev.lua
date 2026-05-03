3ac798ff50e04fa9efa6801a6a5c0c4f9fad5b90
Jordan King <kingjordan2718@gmail.com>
2026-05-02 23:17:22 -0700

    refactor(nvim): rewrite `lazydev` to use `vim.pack`

--- nvim/.config/nvim/plugin/lazydev.lua
+++ nvim/.config/nvim/plugin/lazydev.lua
@@ -1,11 +1,9 @@
-return {
-	"folke/lazydev.nvim",
-	ft = "lua",
-	opts = {
-		library = {
-			{ path = "${3rd}/luv/library", words = { "vim%.uv" } }, -- Load luvit types when the `vim.uv` word is found
-			"nvim-dap-ui",
-			"nvim-lspconfig/lua/lspconfig",
-		},
+vim.pack.add({ "https://github.com/folke/lazydev.nvim" })
+
+require("lazydev").setup({
+	library = {
+		{ path = "${3rd}/luv/library", words = { "vim%.uv" } }, -- Load luvit types when the `vim.uv` word is found
+		"nvim-dap-ui",
+		"nvim-lspconfig/lua/lspconfig",
 	},
-}
+})
