26298fd2680ee0f6422a9d9960dd7d496111735d
Jordan King <kingjordan2718@gmail.com>
2026-05-03 16:11:29 -0700

    refactor(nvim): rewrite `quicker` to use `vim.pack`

--- nvim/.config/nvim/plugin/quicker.lua
+++ nvim/.config/nvim/plugin/quicker.lua
@@ -1,36 +1,33 @@
-return {
-	"stevearc/quicker.nvim",
-	---@module "quicker"
-	---@type quicker.SetupOptions
+vim.pack.add({ "https://github.com/stevearc/quicker.nvim" })
+
+require("quicker").setup({
 	opts = {
-		opts = {
-			relativenumber = true,
-			number = true,
-			signcolumn = vim.o.signcolumn,
+		relativenumber = true,
+		number = true,
+		signcolumn = vim.o.signcolumn,
+	},
+	highlight = {
+		-- Use treesitter highlighting
+		treesitter = true,
+		-- Use LSP semantic token highlighting
+		lsp = true,
+		-- Load the referenced buffers to apply more accurate highlights (may be slow)
+		load_buffers = true,
+	},
+	keys = {
+		{
+			">",
+			function()
+				require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+			end,
+			desc = "Expand quickfix context",
 		},
-		highlight = {
-			-- Use treesitter highlighting
-			treesitter = true,
-			-- Use LSP semantic token highlighting
-			lsp = true,
-			-- Load the referenced buffers to apply more accurate highlights (may be slow)
-			load_buffers = true,
-		},
-		keys = {
-			{
-				">",
-				function()
-					require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
-				end,
-				desc = "Expand quickfix context",
-			},
-			{
-				"<",
-				function()
-					require("quicker").collapse()
-				end,
-				desc = "Collapse quickfix context",
-			},
+		{
+			"<",
+			function()
+				require("quicker").collapse()
+			end,
+			desc = "Collapse quickfix context",
 		},
 	},
-}
+})
