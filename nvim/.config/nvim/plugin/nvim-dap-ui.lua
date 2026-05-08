8a90cab85a08444ef2ac0bc069c2b70e39fbf86c
Jordan King <kingjordan2718@gmail.com>
2026-05-08 12:08:36 -0700

    refactor(nvim): rewrite `nvim-dap-ui` to use `vim.pack`

--- nvim/.config/nvim/plugin/nvim-dap-ui.lua
+++ nvim/.config/nvim/plugin/nvim-dap-ui.lua
@@ -1,28 +1,25 @@
-return {
-	"rcarriga/nvim-dap-ui",
-	dependencies = {
-		"mfussenegger/nvim-dap",
-		"nvim-neotest/nvim-nio",
-	},
-	lazy = true,
-	config = function()
-		local dap, dapui = require("dap"), require("dapui")
+vim.pack.add({
+	-- Dependencies
+	"https://codeberg.org/mfussenegger/nvim-dap",
+	"https://github.com/nvim-neotest/nvim-nio",
 
-		-- Initialize `dap-ui`
-		dapui.setup()
+	"https://github.com/rcarriga/nvim-dap-ui",
+})
 
-		-- Automatically open/close `dap-ui` based on debugger statuses
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
-	end,
-}
+local dap, dapui = require("dap"), require("dapui")
+
+dapui.setup()
+
+-- Automatically open/close `dap-ui` based on debugger statuses
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	dapui.close()
+end
