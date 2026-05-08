5c68c012784052c1b3395291f03fae8e82e7b77d
Jordan King <kingjordan2718@gmail.com>
2026-05-08 12:43:38 -0700

    refactor(nvim)!: rewrite `nvim-jdtls` to use `vim.pack`
    This plugin is lazy-loaded; it'll only be activated in "Java" filetypes.
    
    I've also changed the `extract_constant` LSP keymap to be `grC` instead
    of `grc` since `grc` is now used for changing color presentation.

--- nvim/.config/nvim/plugin/nvim-jdtls.lua
+++ nvim/.config/nvim/plugin/nvim-jdtls.lua
@@ -1,19 +1,24 @@
-return {
-	"mfussenegger/nvim-jdtls",
-	ft = "java",
-	dependencies = { "mfussenegger/nvim-dap" },
-	config = function()
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "java" },
+	callback = function()
+		vim.pack.add({
+			-- Dependencies
+			"https://codeberg.org/mfussenegger/nvim-dap",
+
+			"https://codeberg.org/mfussenegger/nvim-jdtls",
+		})
+
 		local jdtls = require("jdtls")
 
 		-- Additional LSP actions
 		vim.keymap.set("n", "grs", jdtls.super_implementation, { desc = "Java: Jump to method super implementation" })
 		vim.keymap.set("n", "grv", jdtls.extract_variable, { desc = "Java: Extract variable" })
-		vim.keymap.set("n", "grc", jdtls.extract_constant, { desc = "Java: Extract constant" })
+		vim.keymap.set("n", "grC", jdtls.extract_constant, { desc = "Java: Extract constant" })
 		vim.keymap.set("n", "grm", jdtls.extract_method, { desc = "Java: Extract method" })
 		vim.keymap.set("v", "grv", function()
 			jdtls.extract_variable({ visual = true })
 		end, { desc = "Java: Extract Variable" })
-		vim.keymap.set("v", "grc", function()
+		vim.keymap.set("v", "grC", function()
 			jdtls.extract_constant({ visual = true })
 		end, { desc = "Java: Extract constant" })
 		vim.keymap.set("v", "grm", function()
@@ -24,4 +29,4 @@ return {
 		vim.keymap.set("n", "<leader>dC", jdtls.test_class, { desc = "Java - Run test class above cursor" })
 		vim.keymap.set("n", "<leader>dm", jdtls.test_nearest_method, { desc = "Java - Run test method above cursor" })
 	end,
-}
+})
