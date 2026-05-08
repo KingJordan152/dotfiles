5621513b9636723367191445913e46cc9633c2ca
Jordan King <kingjordan2718@gmail.com>
2026-05-08 11:02:47 -0700

    refactor(nvim): rewrite `todo-comments` to use `vim.pack`

--- nvim/.config/nvim/plugin/todo-comments.lua
+++ nvim/.config/nvim/plugin/todo-comments.lua
@@ -1,41 +1,41 @@
-return {
-	"folke/todo-comments.nvim",
-	dependencies = { "folke/snacks.nvim", "nvim-lua/plenary.nvim" },
-	event = "VeryLazy",
+vim.pack.add({
+	-- Dependencies
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/folke/snacks.nvim",
 
-	-- TODO: Example :)
+	"https://github.com/folke/todo-comments.nvim",
+})
 
-	config = function()
-		require("todo-comments").setup({
-			keywords = {
-				TODO = { icon = " ", color = "hint" },
-				NOTE = { icon = " ", color = "info", alt = { "INFO" } },
-				TICKET = { icon = " ", color = "ticket", alt = { "JIRA", "LINEAR" } },
-			},
-			highlight = {
-				multiline = false,
-			},
-			colors = {
-				ticket = { "#339CFF" },
-			},
-		})
+-- TODO: Example :)
 
-		vim.keymap.set("n", "<leader>st", function()
-			---@diagnostic disable-next-line: undefined-field
-			Snacks.picker.todo_comments()
-		end, { desc = "All Todo Comments" })
+require("todo-comments").setup({
+	keywords = {
+		TODO = { icon = " ", color = "hint" },
+		NOTE = { icon = " ", color = "info", alt = { "INFO" } },
+		TICKET = { icon = " ", color = "ticket", alt = { "JIRA", "LINEAR" } },
+	},
+	highlight = {
+		multiline = false,
+	},
+	colors = {
+		ticket = { "#339CFF" },
+	},
+})
 
-		vim.keymap.set("n", "<leader>sT", function()
-			---@diagnostic disable-next-line: undefined-field
-			Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
-		end, { desc = "Todo/Fix/Fixme Comments" })
+vim.keymap.set("n", "<leader>st", function()
+	---@diagnostic disable-next-line: undefined-field
+	require("snacks").picker.todo_comments()
+end, { desc = "All Todo Comments" })
 
-		vim.keymap.set("n", "]t", function()
-			require("todo-comments").jump_next()
-		end, { desc = "Next todo comment" })
+vim.keymap.set("n", "<leader>sT", function()
+	---@diagnostic disable-next-line: undefined-field
+	require("snacks").picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+end, { desc = "Todo/Fix/Fixme Comments" })
 
-		vim.keymap.set("n", "[t", function()
-			require("todo-comments").jump_prev()
-		end, { desc = "Previous todo comment" })
-	end,
-}
+vim.keymap.set("n", "]t", function()
+	require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+	require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
