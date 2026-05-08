4d0c712730e4535e69ec9be828b67a7c8b08e6f8
Jordan King <kingjordan2718@gmail.com>
2026-05-08 12:30:06 -0700

    refactor(nvim): rewrite `nvim-dap-go` to use `vim.pack`
    This plugin is lazy-loaded; it'll only be activated in "Go" filetypes.

--- nvim/.config/nvim/plugin/nvim-dap-go.lua
+++ nvim/.config/nvim/plugin/nvim-dap-go.lua
@@ -1,12 +1,17 @@
-return {
-	"leoluz/nvim-dap-go",
-	ft = "go",
-	dependencies = { "mfussenegger/nvim-dap" },
-	config = function()
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "go", "gomod", "gowork", "gotmpl" },
+	callback = function()
+		vim.pack.add({
+			-- Dependencies
+			"https://codeberg.org/mfussenegger/nvim-dap",
+
+			"https://github.com/leoluz/nvim-dap-go",
+		})
+
 		local dap_go = require("dap-go")
 		dap_go.setup()
 
 		vim.keymap.set("n", "<leader>dt", dap_go.debug_test, { desc = "Go - Debug method closest to cursor" })
 		vim.keymap.set("n", "<leader>dT", dap_go.debug_last_test, { desc = "Go - Debug last test" })
 	end,
-}
+})
