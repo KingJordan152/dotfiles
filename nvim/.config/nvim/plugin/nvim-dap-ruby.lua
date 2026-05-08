f123c0ed06f9972114ed15ea30781e6af6291d1f
Jordan King <kingjordan2718@gmail.com>
2026-05-08 12:39:32 -0700

    refactor(nvim): rewrite `nvim-dap-ruby` to use `vim.pack`
    This plugin is lazy-loaded; it'll only be activated in "Ruby" filetypes.

--- nvim/.config/nvim/plugin/nvim-dap-ruby.lua
+++ nvim/.config/nvim/plugin/nvim-dap-ruby.lua
@@ -1,6 +1,13 @@
-return {
-	"suketa/nvim-dap-ruby",
-	ft = "ruby",
-	dependencies = { "mfussenegger/nvim-dap" },
-	opts = {},
-}
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "ruby", "eruby" },
+	callback = function()
+		vim.pack.add({
+			-- Dependencies
+			"https://codeberg.org/mfussenegger/nvim-dap",
+
+			"https://github.com/suketa/nvim-dap-ruby",
+		})
+
+		require("dap-ruby").setup()
+	end,
+})
