ef9e92cfb5062a04376d0618b0603e785be57f4d
Jordan King <kingjordan2718@gmail.com>
2026-05-21 09:45:53 -0700

    feat(nvim): add various `spell` options for when `vim.o.spell = true`

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -22,6 +22,8 @@ vim.o.linebreak = true -- When `wrap` is true, causes full words to wrap rather 
 vim.o.winborder = "rounded" -- Make all floating windows have the "rounded" border.
 vim.o.foldlevelstart = 99 -- Opens all folds created by Treesitter immediately (VS Code behavior)
 vim.o.swapfile = false -- Disable swapfile creation
+vim.o.spelllang = "en_us" -- Set the language `spell` uses to U.S. English
+vim.o.spelloptions = "camel" -- Spellcheck the individual parts of a camelCased word
 vim.g.markdown_recommended_style = 0 -- Disable all preset Markdown styles (they overwrite the ones set here)
 
 -- Defines the characters to use for different special lines throughout Neovim
