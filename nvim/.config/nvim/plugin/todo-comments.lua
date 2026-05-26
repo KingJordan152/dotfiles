42aec1a6563648e0287562f17c56d79bbf900518
Jordan King <kingjordan2718@gmail.com>
2026-05-26 16:57:00 -0700

    refactor(nvim): simplify keymap declarations in `todo-comments`

--- nvim/.config/nvim/plugin/todo-comments.lua
+++ nvim/.config/nvim/plugin/todo-comments.lua
@@ -24,15 +24,11 @@ require("todo-comments").setup({
   },
 })
 
-vim.keymap.set("n", "<leader>st", function()
-  ---@diagnostic disable-next-line: undefined-field
-  require("snacks").picker.todo_comments()
-end, { desc = "All Todo Comments" })
+---@diagnostic disable-next-line: undefined-field
+vim.keymap.set("n", "<leader>st", function() Snacks.picker.todo_comments() end, { desc = "All Todo Comments" })
 
-vim.keymap.set("n", "<leader>sT", function()
-  ---@diagnostic disable-next-line: undefined-field
-  require("snacks").picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
-end, { desc = "Todo/Fix/Fixme Comments" })
+---@diagnostic disable-next-line: undefined-field
+vim.keymap.set("n", "<leader>sT", function() Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, { desc = "Todo/Fix/Fixme Comments" })
 
 vim.keymap.set("n", "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" })
 
