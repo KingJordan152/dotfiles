d9b3140bc159cd52449d5b25059372f3e0bc3bd2
Jordan King <kingjordan2718@gmail.com>
2026-06-08 21:48:44 -0700

    feat(nvim): add new keymaps for commenting inspired by Comment.nvim
    Since Comment.nvim is now largely considered to be unmaintained and
    obsolete, I've decided to port over some of its helpful keymaps nearly
    directly from its source code.
    
    Currently, they use `ts_context_commentstring` directly. I would like to
    make these functions more generic and just use `vim.bo.commentstring`,
    but that won't work until I'm able to use that plugin with Neovim's
    native commenting system.
    
    The native commenting system straight-up doesn't work with TSX due to
    their Treesitter capture logic, and that doesn't seem like it'll change
    at least until Treesitter queries are upstreamed.

--- nvim/.config/nvim/plugin/mini-comment.lua
+++ nvim/.config/nvim/plugin/mini-comment.lua
@@ -16,3 +16,58 @@ require("mini.comment").setup({
     custom_commentstring = function() return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring end,
   },
 })
+
+-- ==== Keymap Helper Functions ====
+
+---Splits the given `commentstring` into its left and right side.
+---The user's comment is expected to go between both sides.
+---@param commentstring string String that will be decomposed. Must contain a special `%s` sequence.
+---@return string|nil l_cms, string|nil r_cms
+local function decompose_commentstring(commentstring)
+  if not commentstring then
+    return
+  end
+
+  local l_cms, r_cms = string.match(commentstring, "(.*)%%s(.*)")
+  l_cms = vim.trim(l_cms) .. " "
+  r_cms = vim.trim(r_cms)
+
+  -- Handle case where there is a right commentstring (i.e., block comments)
+  if #r_cms ~= 0 then
+    r_cms = " " .. r_cms
+  end
+
+  return l_cms, r_cms
+end
+
+---Insert a new comment either above or below the cursor's current position.
+---@param offset integer Distance away from the cursor's current position.
+---`0` is immediately below; `1` is immediately above
+local function insert_comment_above_or_below(offset)
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  local comment_row = row + offset
+  local l_cms, r_cms = decompose_commentstring(require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring)
+
+  vim.api.nvim_buf_set_lines(0, comment_row, comment_row, false, { l_cms .. r_cms }) -- Add new line with commentstring
+  vim.api.nvim_win_set_cursor(0, { comment_row + 1, 0 }) -- Move cursor to the new line
+  vim.api.nvim_command("normal! ==") -- Set the correct indentation level
+  vim.api.nvim_win_set_cursor(0, { comment_row + 1, #vim.api.nvim_get_current_line() - #r_cms - 1 }) -- Move cursor inside the commentstring
+  vim.api.nvim_feedkeys("a", "ni", true) -- Enter Insert mode
+end
+
+---Insert a new comment at the end of the current line.
+local function insert_comment_eol()
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  local l_cms, r_cms = decompose_commentstring(require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring)
+
+  vim.api.nvim_set_current_line(vim.api.nvim_get_current_line() .. " " .. l_cms .. r_cms) -- Add commenstring to EOL
+  vim.api.nvim_command("normal! ==") -- If on new line, indent it
+  vim.api.nvim_win_set_cursor(0, { row, #vim.api.nvim_get_current_line() - #r_cms - 1 }) -- Move cursor to appropriate location
+  vim.api.nvim_feedkeys("a", "ni", true) -- Enter Insert mode
+end
+
+-- ==== Keymaps (Inspired by Comment.nvim) ====
+
+vim.keymap.set("n", "gco", function() insert_comment_above_or_below(0) end, { desc = "Insert comment below" })
+vim.keymap.set("n", "gcO", function() insert_comment_above_or_below(-1) end, { desc = "Insert comment above" })
+vim.keymap.set("n", "gcA", insert_comment_eol, { desc = "Insert comment at the end of the line" })
